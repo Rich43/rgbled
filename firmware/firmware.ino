a1fe606e6b5bbadddfd73aef66c510ec73ad9281
Richie Ward <RichieS@GMail.com>
2019-02-02 16:14:24 +0000

    Add on off switch

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -2,14 +2,11 @@
 #include <ArduinoJson.h>
 #include <EEPROM.h>
 
-// {"r":10,"g":30,"b":40}
-
 DynamicJsonDocument doc(1024);
 
 #define NUM_LEDS 1
 #define DATA_PIN 2
 #define CLOCK_PIN 3
-#define BRIGHTNESS  250
 
 CRGB leds[NUM_LEDS];
 
@@ -21,8 +18,6 @@ TBlendType    currentBlending;
 extern CRGBPalette16 myRedWhiteBluePalette;
 extern const TProgmemPalette16 myRedWhiteBluePalette_p PROGMEM;
 
-int integerFromPC = 0;
-
 void setRGB(int red, int green, int blue) {
     leds[0].red   = red;
     leds[0].green = green;
@@ -31,6 +26,10 @@ void setRGB(int red, int green, int blue) {
 }
 
 void setup() {
+    if (!(EEPROM.read(4) == 0 || EEPROM.read(4) == 1)) {
+        EEPROM.write(4, 1);
+    }
+
     FastLED.addLeds<P9813, DATA_PIN, CLOCK_PIN, RGB>(leds, NUM_LEDS).setCorrection( TypicalLEDStrip );
 
     Serial.begin(115200);
@@ -62,36 +61,46 @@ void loop()
     String g = obj["g"];
     String b = obj["b"];
     String br = obj["br"];
+    String onoff = obj["onoff"];
     bool dataChanged = false;
 
-    if (r != "null") {
-        leds[0].red   = r.toInt();
+    if (onoff != "null") {
         dataChanged = true;
-        EEPROM.write(0, leds[0].red);
+        EEPROM.write(4, onoff.toInt() == 1 ? 1 : 0);
     }
-    if (g != "null") {
-        leds[0].green = g.toInt();
-        dataChanged = true;
-        EEPROM.write(1, leds[0].green);
-    }
-    if (b != "null") {
-        leds[0].blue  = b.toInt();
-        dataChanged = true;
-        EEPROM.write(2, leds[0].blue);
-    }
-    if (br != "null") {
-        FastLED.setBrightness(br.toInt());
-        dataChanged = true;
-        EEPROM.write(3, br.toInt());
+
+    if (EEPROM.read(4) == 1) {
+        if (r != "null") {
+            leds[0].red   = r.toInt();
+            dataChanged = true;
+            EEPROM.write(0, leds[0].red);
+        }
+        if (g != "null") {
+            leds[0].green = g.toInt();
+            dataChanged = true;
+            EEPROM.write(1, leds[0].green);
+        }
+        if (b != "null") {
+            leds[0].blue  = b.toInt();
+            dataChanged = true;
+            EEPROM.write(2, leds[0].blue);
+        }
+        if (br != "null") {
+            FastLED.setBrightness(br.toInt());
+            dataChanged = true;
+            EEPROM.write(3, br.toInt());
+        }
+        reset();
+    } else {
+        FastLED.setBrightness(0);
+        setRGB(0,0,0);
     }
     if (dataChanged) {
         status();
     }
-
-    FastLED.show();
     FastLED.delay(1000 / UPDATES_PER_SECOND);
 }
 
 void status() {
-    Serial.println(String(leds[0].red) + "," + String(leds[0].green) + "," + String(leds[0].blue) + "," + String(FastLED.getBrightness()));
+    Serial.println(String(leds[0].red) + "," + String(leds[0].green) + "," + String(leds[0].blue) + "," + String(FastLED.getBrightness()) + "," + String(EEPROM.read(4)));
 }
